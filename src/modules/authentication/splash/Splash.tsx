6d2ff7d3a031e88470aab7572538b2a83cee1aa8
Punit Vajpeyi <162538998+hiren-wemotive@users.noreply.github.com>
2024-06-06 15:36:33 +0530

    update splash.tsx

--- src/modules/authentication/splash/Splash.tsx
+++ src/modules/authentication/splash/Splash.tsx
@@ -167,9 +167,7 @@ const Splash: React.FC<Splash> = ({navigation}) => {
           'Alert',
           'Please setup the device lock to access this application',
           'Ok',
-          () => {
-            processLink();
-          },
+          () => {},
         );
       } else {
         if (JailMonkey.isJailBroken()) {
